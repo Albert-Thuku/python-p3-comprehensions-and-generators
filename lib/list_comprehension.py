d92c1724f651145e2bf0f8e3d2eb8b86bb980ece
Albert Thuku <athuku02@gmail.com>
2023-05-17 19:59:21 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+import ipdb
 
 def return_evens(num_list):
-    pass
+    evens = []
+    for num in num_list:
+        if num % 2 == 0:
+            evens.append(num)
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    exclamated = []
+    for sentence in sentence_list:
+        modified_sentence = sentence + '!'
+        exclamated.append(modified_sentence)
+    return exclamated
